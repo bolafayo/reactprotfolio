dbcdd905ac8f8a2a8b8d9e1e2401a4bba0c8d603
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-08-25 09:11:27 +0100

    created another distription

--- src/data/user.js
+++ src/data/user.js
@@ -2,7 +2,7 @@ const INFO = {
 	main: {
 		title: "my protfolio",
 		name: "Olufayo Bolade Taiwo.",
-		email: "boladeolufayo@gmail.com",
+		email: "mailtoboladeolufayo@gmail.com",
 	
 	},
 
@@ -10,8 +10,7 @@ const INFO = {
 		twitter: "https://twitter.com/Bola_fayo/",
 		github: "https://github.com/bolafayo/",
 		linkedin: "https://linkedin.com/BoladeOlufayo/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
+	
 		
 	},
 
@@ -22,9 +21,10 @@ const INFO = {
 	},
 
 	about: {
-		title: "I’m a Software Engineering student .",
+		title: "I’m a Software Engineer .",
 		description:
-			"I've worked on a variety of projects over the months and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+		
+			" I’m Bolade Olufayo. My journey into tech didn’t start with a laptop, it started behind a salon chair. For years, I worked as a professional hairstylist, helping clients bring their ideas to life through creativity, precision, and a lot of problem-solving. Along the way, I learned how to really listen, translate visions into reality, and manage multiple projects under pressure, skills that turned out to be just as valuable in software development. When I transitioned into Software Engineering, I discovered that coding is more than just writing lines of code, it’s about solving problems and creating experiences that people actually enjoy using. Today, I’m a Frontend Developer passionate about building applications that are clean, user-friendly, and accessible. I enjoy bringing ideas to life with HTML, CSS, JavaScript, and Tailwind CSS, and I’m constantly learning new tools and technologies to sharpen my craft . Beyond the code, I’m passionate about encouraging more women to explore opportunities in tech. I believe that creativity, empathy, and collaboration are just as important as technical skills, and I bring all of that to every project I work on. I've worked on a variety of projects over the months and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
 
 	articles: {
@@ -35,45 +35,151 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
-			description:
-				"Responsive, User-Friendly E-Commerce Website Built with Performance in Mind.",
+			title: "E-commerce",
+			shortDescription: "Responsive, user-friendly e-commerce website.",
+  description: "Responsive, User-Friendly E-Commerce Website Built with Performance in Mind.",
+  fullDescription: `
+  <div style="text-align: center; margin:40px ;">
+    <img src="/pho1.png" alt=" Homepage" width="600"/>
+  </div>
+  <p>A modern e-commerce web application where users can  browse products, view details, choose sizes, register/log in, add items to their cart, and check out seamlessly. The project leverages API integration, local storage persistence, and a clean, responsive design for a smooth shopping experience
+  Product listing with images, titles, and prices, Product detail page with size selection ,add to cart (with authentication required),cart management (remove items, clear cart, calculate total price),user authentication (registration & login),localStorage API for cart and session persistence,responsive design with Grid & CSS3, toast notifications for feedback (VanillaToasts), tools used(HTML5, CSS3 / Flexbox, JavaScript (Vanilla),Font Awesome,
+  
+  -API integration (Fake Store API) :</p>
+  <p>Live Demo: 
+  <a href="https://ecommerce-six-topaz-75.vercel.app/" target="_blank" rel="noopener noreferrer">
+	https://ecommerce-six-topaz-75.vercel.app/
+  </a>
+</p>
+  <ul>
+    <li>Browse products</li>
+    <li>View details and choose sizes</li>
+    <li>Register and log in</li>
+    <li>Add items to their cart</li>
+    <li>Checkout seamlessly</li>
+  </ul>
+  <p> Built with API integration, local storage persistence, and a clean responsive design.</p>
+`,
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com/bolafayo/ecommerce",
 		},
 
 		{
-			title: "Project 2",
-			description:
-				"A clean, responsive blog application featuring dynamic date rendering, comment and like systems with icons, engaging reading experience..",
+			title: "Personal-blog",
+			shortDescription: "A clean, responsive blog application featuring dynamic date rendering, comment and like systems with icons, engaging reading experience..",
+  fullDescription: `
+  <div style="text-align: center; margin:40px ;">
+    <img src="/pho2.png" alt=" Homepage" width="600"/>
+  </div>
+  <p>A simple yet interactive personal blog application where users can read posts, leave comments, like content, share posts, and subscribe to a newsletter. Designed with clean UI/UX and powered by HTML, CSS, and JavaScript.Dynamic Date Rendering (using JavaScript), Comment System with delete option, Like Button with icon, Share Post Section (Facebook, Twitter, WhatsApp, Copy Link), Newsletter Subscription Form (stored in LocalStorage,Responsive Design with clean styling,LocalStorage Integration to persist user input .Tools(HTML5, CSS3 / Flexbox, JavaScript (DOM Manipulation, LocalStorage), Font Awesome for icons) </p>
+  <p>Live Demo: 
+  <a href="https://personalblog-one-beige.vercel.app/" target="_blank" rel="noopener noreferrer">
+  https://personalblog-one-beige.vercel.app/
+  </a>
+</p>
+  <ul>
+    <li>DOM manipulation with JavaScript</li>
+    <li>Handling form inputs and saving data to LocalStorage</li>
+    <li>Implementing interactive UI with icons</li>
+    <li>Creating toggle menus with event listeners</li>
+    <li>Structuring a personal project for a portfolio</li>
+  </ul>
+ 
+`,
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com/bolafayo/personalblog",
 		},
 
 		{
-			title: "Project 3",
-			description:
-				"A dynamic recipe web app with search, filters, ratings, favorites, and personalized dashboards for diet, allergies, cuisines, and goals..",
+			
+			title:"recipe-website",
+			shortDescription: "A dynamic recipe web app with search, filters, ratings, favorites, and personalized dashboards for diet, allergies, cuisines, and goals...",
+  fullDescription: `
+  <div style="text-align: center; margin:40px ;">
+    <img src="/pho3.png" alt="Homepage" width="600"/>
+  </div>
+  <p>A recipe discovery web application where users can search, filter by diet, allergies, cuisines, and goals, mark favorites, and view detailed cooking instructions.Product listing with images, titles, and prices, Product detail page with size selection, add to cart (with authentication required), cart management (remove items, clear cart, calculate total price), user authentication (registration & login), localStorage API for cart and session persistence, responsive design with Grid & CSS3, toast notifications for feedback (VanillaToasts). Tools(React, VanillaToasts, Font Awesome, CSS3 / Flex, LocalStorage API, DummyJSON Recipes API) </p>
+
+  <p>Live Demo: 
+  <a href="https://recipeswebsite-psi.vercel.app/" target="_blank" rel="noopener noreferrer">
+  https://recipeswebsite-psi.vercel.app/
+  </a>
+</p>
+  <ul>
+    <li>Working with APIs</li>
+    <li>State management through localStorage.</li>
+    <li>Error handling and user feedback</li>
+    <li>Dynamic UI challenges</li>
+    <li>Patience and persistence</li>
+  </ul>
+ 
+`,
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com/bolafayo/recipeswebsite",
+
 		},
 
 		{
-			title: "Project 4",
-			description:
-				"Building the Shop Monogram Project: A Modern E-Commerce Showcase.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			
+			title: "Hair-salon-website",
+			shortDescription: "A modern and interactive Hair Salon website built with React (Vite) and TypeScript, designed to showcase salon services, stylists, and allow clients to book appointments.",
+  fullDescription: `
+  <div style="text-align: center; margin:40px ;">
+    <img src="/pho5.png" alt=" Homepage" width="600"/>
+  </div>
+  <p>The app features dynamic service navigation, an about section with images, stylist profiles, and a contact form integrated with localStorage for appointment tracking. Designed with a focus on clean UI/UX, dark & light mode support, and smooth navigation, this project demonstrates the ability to build a responsive, client-focused application with real-world usability.Navigation Bar with smooth scrolling and page routing
+  Services Page: Clickable service categories (Cut & Styling, Coloring, Treatments, etc.) about Page: Rich descriptions with multiple images
+  Stylists Section: Carousel of expert stylists with profile details and notifications on selection, book Appointment Form: Saves data in localStorage and shows confirmation alerts, back Arrow Navigation on each page to return to Home</p>
+  <p>Live Demo: 
+  <a href="https://vercel.com/bolade-olufayos-projects/hairsalaon/FHh7myn3KWUxNuu7E3UMKVsXA3DH" target="_blank" rel="noopener noreferrer">
+  https://vercel.com/bolade-olufayos-projects/hairsalaon/FHh7myn3KWUxNuu7E3UMKVsXA3DH
+  </a>
+</p>
+  <ul>
+    <li>React (Vite) – Frontend framework</li>
+    <li>TypeScript – Static typing</li>
+    <li>TailwindCSS – Styling and responsive design</li>
+    <li>Lucide React – Icon system</li>
+    <li>LocalStorage API – Data persistence</li>
+	<li>React Router – Page navigation</li>
+  </ul>
+  
+`,
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com/bolafayo/shopmonogram",
+			link: "https://github.com/bolafayo/hairsalaon",
+
 		},
 
 		{
-			title: "Project 5",
-			description:
-				"Todo List App Built with HTML, CSS, and JavaScript to keep productivity simple and engaging, track tasks, filter progress.",
+		
+			title: "Todolist-app",
+			shortDescription: "Todo List App Built with HTML, CSS, and JavaScript to keep productivity simple and engaging, track tasks, filter progress.",
+  fullDescription: `
+  <div style="text-align: center; margin:40px ;">
+    <img src="/pto4.png" alt=" Homepage" width="600"/>
+  </div>
+  <p>A minimal, interactive productivity tool that helps users create, track, and manage tasks with ease.
+  This project was built using HTML, CSS, and Vanilla JavaScript to strengthen skills in DOM manipulation, event handling, and state management.Add complete, and delete tasks, filter tasks: All | Active | Completed, real-time progress bar to visualize task completion
+  Simple and responsive design, motivational interface for productivity. TOOLS(HTML5, CSS3, JavaScript) </p>
+  
+  <p>Live Demo: 
+  <a href="https://todolist-five-xi-20.vercel.app/" target="_blank" rel="noopener noreferrer">
+  https://todolist-five-xi-20.vercel.app/
+  </a>
+</p>
+  <ul>
+    <li>DOM manipulation with JavaScript</li>
+    <li>Handling form inputs and saving data to LocalStorage</li>
+    <li>Implementing interactive UI with icons</li>
+    <li>Creating toggle menus with event listeners</li>
+    <li>Structuring a personal project for a portfolio</li>
+  </ul>
+ 
+`,
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com/bolafayo/todolist",
