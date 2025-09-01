7709af880768d47008c5c72391f99120342f39f4
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-09-01 07:12:57 +0100

    added a link a tag to the section

--- src/components/projects/allProjects.jsx
+++ src/components/projects/allProjects.jsx
@@ -1,87 +1,84 @@
-// import React from "react";
-
+// import React, { useState } from "react";
 // import Project from "./project";
-
 // import INFO from "../../data/user";
-
 // import "./styles/allProjects.css";
 
 // const AllProjects = () => {
-// 	return (
-// 		<div className="all-projects-container">
-// 			{INFO.projects.map((project, index) => (
-// 				<div className="all-projects-project" key={index}>
-// 					<Project
-// 						logo={project.logo}
-// 						title={project.title}
-// 						description={project.description}
-// 						linkText={project.linkText}
-// 						link={project.link}
-// 					/>
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
+//   const [selectedProject, setSelectedProject] = useState(null);
+
+//   return (
+//     <div className="all-projects-container">
+    
+//       {INFO.projects.map((project, index) => (
+//         <div
+//           className="all-projects-project"
+//           key={index}
+//           onClick={() => setSelectedProject(project)}
+//           style={{ cursor: "pointer" }}
+//         >
+//           <Project
+//             logo={project.logo}
+//             title={project.title}
+//             description={project.shortDescription} 
+//           />
+//         </div>
+//       ))}
+
+     
+//       {selectedProject && (
+//         <div className="project-details-overlay">
+//           <div className="project-details">	
+//             <button
+//               className="close-btn"
+//               onClick={() => setSelectedProject(null)}
+//             >
+//               ✖
+//             </button>
+//             <h2>{selectedProject.title}</h2>
+//             <img
+//               src={selectedProject.logo}
+//               alt={selectedProject.title}
+//               className="project-logo"
+//             />
+//           <div
+//   className="project-readme"
+//   dangerouslySetInnerHTML={{ __html: selectedProject.fullDescription }}
+// 			/>
+//             <a
+//               href={selectedProject.link}
+//               target="_blank"
+//               rel="noreferrer"
+//               className="project-link"
+//             >
+//               {selectedProject.linkText}
+//             </a>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
 // };
 
 // export default AllProjects;
-import React, { useState } from "react";
+
+import React from "react";
 import Project from "./project";
 import INFO from "../../data/user";
 import "./styles/allProjects.css";
 
 const AllProjects = () => {
-  const [selectedProject, setSelectedProject] = useState(null);
-
   return (
     <div className="all-projects-container">
-      {/* List of projects */}
       {INFO.projects.map((project, index) => (
-        <div
-          className="all-projects-project"
-          key={index}
-          onClick={() => setSelectedProject(project)}
-          style={{ cursor: "pointer" }}
-        >
+        <div className="all-projects-project" key={index}>
           <Project
+            id={index} // 🔑 pass index as id
             logo={project.logo}
             title={project.title}
-            description={project.shortDescription} // 👈 only short desc shows here
+            description={project.shortDescription}
           />
         </div>
       ))}
-
-      {/* Modal for full details */}
-      {selectedProject && (
-        <div className="project-details-overlay">
-          <div className="project-details">	
-            <button
-              className="close-btn"
-              onClick={() => setSelectedProject(null)}
-            >
-              ✖
-            </button>
-            <h2>{selectedProject.title}</h2>
-            <img
-              src={selectedProject.logo}
-              alt={selectedProject.title}
-              className="project-logo"
-            />
-          <div
-  className="project-readme"
-  dangerouslySetInnerHTML={{ __html: selectedProject.fullDescription }}
-			/>
-            <a
-              href={selectedProject.link}
-              target="_blank"
-              rel="noreferrer"
-              className="project-link"
-            >
-              {selectedProject.linkText}
-            </a>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
