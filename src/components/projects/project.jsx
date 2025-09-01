fb377f0e0dd30276ed0d4826b8fea96117681740
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-09-01 07:14:52 +0100

    added a click button

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -6,30 +6,27 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 import "./styles/project.css";
 
 const Project = (props) => {
-	const { logo, title, description, linkText, link } = props;
+  const { id, logo, title, description } = props;
 
-	return (
-		<React.Fragment>
-			<div className="project">
-				<Link to={link}>
-					<div className="project-container">
-						<div className="project-logo">
-							<img src={logo} alt="logo" />
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-description">{description}</div>
-						<div className="project-link">
-							<div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
-
-							<div className="project-link-text">{linkText}</div>
-						</div>
-					</div>
-				</Link>
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <div className="project">
+      <Link to={`/projects/${id}`}>
+        <div className="project-container">
+          <div className="project-logo">
+            <img src={logo} alt={title} />
+          </div>
+          <div className="project-title">{title}</div>
+          <div className="project-description">{description}</div>
+          <div className="project-link">
+            <div className="project-link-icon">
+              <FontAwesomeIcon icon={faLink} />
+            </div>
+            <div className="project-link-text">View Details</div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Project;
